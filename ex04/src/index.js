a9fd29fbbfeffb58744952992d21bc85bc08038c
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-07 22:10:51 +0200

    Add Exercise 04

---
+++ ex04/src/index.js
@@ -0,0 +1,22 @@
+var temps = [
+[31, 32, 19, 37],
+[29, 27, 55, 36],
+[17, 27, 55, 36],
+[29, 52, 21, 64],
+[91, 27, 31, 61]
+];
+
+function myArrayFunction(temps){
+var averageDayTemp = [];
+for( var i=0; i<5; i++)
+ {   var sum =0;
+     for(var j=0; j< 4; j++){
+         sum += temps[i][j];
+     }
+     averageDayTemp[i] = sum/4;
+ }
+return averageDayTemp;
+}
+
+console.log(myArrayFunction(temps));
+module.exports = myArrayFunction;
