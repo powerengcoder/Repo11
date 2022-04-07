4c981cdcb5aaea07b20e507bdbdc32155144f0a8
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-07 22:38:37 +0200

    Add Exercise 06

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+var myNestedArray = [["Toblerone", 5], ["Milka", 3]];
+
+function myNestedFunction(myNestedArray){
+
+    return myNestedArray;
+
+}
+
+console.log(myNestedFunction(myNestedArray));
+module.exports =myNestedFunction;
