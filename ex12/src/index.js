24edaaa7d9774f79e781c3fd7e22a74e435a0d07
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-09 23:55:56 +0200

    Makeup ex12

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,13 +1,20 @@
 function splitArrayInGroups(arr, n){
+var res =[];
 var result =[];
 
     for (let i = 0; i < arr.length; i += n) {
         var subArr = arr.slice(i, i + n);
-        result.push(subArr);
+        res.push(subArr);
     }
-
+for (var i=0; i<n; i++){
+    result.push(res[i]);
+}
 return result;
 }
 
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6 ], 3));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8 ], 3));
+console.log(splitArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5 ], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
 module.exports = splitArrayInGroups;
