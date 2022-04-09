7efc0c58a53206942afb31fe1b69b5a075b8ac98
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-09 21:20:30 +0200

    Maekup ex05

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,14 +1,12 @@
 var monitorsListArray = ["Apple", "Peach", "Berry"];
 
-function myMonitorsFunction(monitorsListArray){
-var monitorsList = [];
+function myMonitorsFunction(monitorsListArray) {
+  var monitorsList = [];
 
-for(var i=0; i<3; i++){
-    monitorsList.push([monitorsListArray[i] + ", " + (i+1)]);
-}
-
-return monitorsList;
-
+  for (var i = 0; i < monitorsListArray.length; i++) {
+    monitorsList.push(new Array(monitorsListArray[i], i + 1));
+  }
+  return monitorsList;
 }
 myMonitorsFunction(monitorsListArray);
 console.log(myMonitorsFunction(monitorsListArray));
