3566da4dc8aee90cceb2b8c9bd12a7e3f59017ff
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-09 21:23:57 +0200

    Makeup ex07

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,25 +1,26 @@
 var myArray = [
-[1, 2],
-[3, 4, 5, 6],
-[7, 8, 9]
+  [1, 2],
+  [3, 4, 5, 6],
+  [7, 8, 9],
 ];
 
-function multiplyArrayfunction(array){
-var arr = [];
-var sumAll =0; var productAll =1;
-    for( var i=0; i < array.length; i++)
-     {   var sum =0; var product =1;
-         for(var j=0; j < array[i].length; j++){
-             sum += array[i][j];
-             product *= array[i][j];
-         }
-       sumAll += sum;
-       productAll *=product;
-     }
-     arr.push(productAll,sumAll);
-    return arr;
+function multiplyArrayFunction(array) {
+  var arr = [];
+  var sumAll = 0;
+  var productAll = 1;
+  for (var i = 0; i < array.length; i++) {
+    var sum = 0;
+    var product = 1;
+    for (var j = 0; j < array[i].length; j++) {
+      sum += array[i][j];
+      product *= array[i][j];
     }
+    sumAll += sum;
+    productAll *= product;
+  }
+  arr.push(productAll, sumAll);
+  return arr;
+}
 
-
-console.log(multiplyArrayfunction(myArray));
-module.exports = multiplyArrayfunction;
+console.log(multiplyArrayFunction(myArray));
+module.exports = multiplyArrayFunction;
